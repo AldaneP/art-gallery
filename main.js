00f8092cef85707c6100c000b9943f90a7e8724e
Aldane Purrier <49955250+AldaneP@users.noreply.github.com>
2019-05-15 22:33:33 -0400

    weird black box

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 class Classic {
   constructor(art_url) {
-    this.art_url = url('https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg')
-    this.frame = 'black border'
-    // thing_to_set = condition ? true : false
-    this.art_url ? art_url : 'default_url'
+    this.frame = 'black-border'
+    this.art_url ='https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
+
 }
- }
 
  render() {
-   let el = document.createElement('div') 
+   let el = document.createElement('div')
    el.style.width = '100px'
    el.style.height = '100px'
-   el.style.backgroundColor = 'black'
+   div.style.border = '2px solid black'
    document.body.appendChild(el)
  }
- }
+}
+
+// render() {
+//   let img = new image()
+//   div.style.backgroundImage = url('https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg')
+//   )
+
+var art1 = new Classic ("http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg")
+
+class Meme {
+  constructor(art_url) {
+    this.frame = 'blue-border'
+    this.art_url ='https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
+}
+
+render() {
+  let el = document.createElement('div')
+  el.style.width = '100px'
+  el.style.height = '100px'
+  el.style.backgroundColor = 'black'
+  document.body.appendChild(el)
+}
+}
+
+var meme1 = new Meme('http://iO.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg')
+meme1.render()
